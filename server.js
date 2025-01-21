ad9d3d1770a5d406e6b7896b9cde0823dd8bcacc
OmarAlmoussa <omaralmousa153@gmail.com>
2025-01-21 01:10:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/team', teamRoutes);
 
 // Database Connection
 mongoose
-    .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error('MongoDB Connection Error:', err));
 
